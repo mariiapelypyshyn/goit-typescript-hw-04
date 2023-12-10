38d1ebc7b73c80cb0f59e33216cc3cf3d54cac68
mariiapelypyshyn <maria.peleshko97@gmail.com>
2023-12-10 19:41:08 +0200

    homework 4

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -1,27 +1,29 @@
-import React, { createContext, useMemo, useState, useContext } from "react";
-import noop from "lodash/noop";
+import React, { createContext, useMemo, useState, useContext, ReactNode } from "react";
+// import noop from "lodash/noop";
 
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
 
-// Додати тип Menu Selected
-
-const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
-});
-
-// Додайте тип MenuAction
-
-const MenuActionContext = createContext<MenuAction>({
-  onSelectedMenu: noop,
-});
-
-type PropsProvider = {
-  children; // Додати тип для children
+type SelectedMenu = {
+  id?: MenuIds;
 };
 
+type MenuSelected = {
+  selectedMenu: SelectedMenu;
+};
+
+type MenuAction = {
+  onSelectedMenu: (selectedMenu: SelectedMenu) => void;
+};
+
+type PropsProvider = {
+  children: ReactNode;
+};
+
+const MenuSelectedContext = createContext<MenuSelected | undefined>(undefined);
+const MenuActionContext = createContext<MenuAction | undefined>(undefined);
+
 function MenuProvider({ children }: PropsProvider) {
-  // Додати тип для SelectedMenu він повинен містити { id }
   const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
 
   const menuContextAction = useMemo(
@@ -48,19 +50,22 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[];
 };
 
 function MenuComponent({ menus }: PropsMenu) {
-  const { onSelectedMenu } = useContext(MenuActionContext);
-  const { selectedMenu } = useContext(MenuSelectedContext);
+  const { onSelectedMenu } = useContext<MenuAction>(
+    MenuActionContext as React.Context<MenuAction>
+  )!;
+  const { selectedMenu } = useContext<MenuSelected>(
+    MenuSelectedContext as React.Context<MenuSelected>
+  )!;
 
   return (
     <>
       {menus.map((menu) => (
         <div key={menu.id} onClick={() => onSelectedMenu({ id: menu.id })}>
-          {menu.title}{" "}
-          {selectedMenu.id === menu.id ? "Selected" : "Not selected"}
+          {menu.title} {selectedMenu.id === menu.id ? "Selected" : "Not selected"}
         </div>
       ))}
     </>
